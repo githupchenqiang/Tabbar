6067429d13ba34743ca435e79c6d3b555c49e515
12726陈强 <chen.qiang@ampsky.com>
2017-11-23 17:44:01 +0800

    [add]添加gif演示效果

--- CQCustomTabbarControll/AppDelegate.m
+++ CQCustomTabbarControll/AppDelegate.m
@@ -20,7 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     CQTabBarController *tabbar = [[CQTabBarController alloc]init];
-    
+    self.window.backgroundColor = [UIColor whiteColor];
     
     [CQTabBarConfig shared].selectedTextColor = [UIColor orangeColor];
     [CQTabBarConfig shared].textColor = [UIColor grayColor];
@@ -42,7 +42,6 @@
     [self.window makeKeyAndVisible];
     return YES;
 }
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ##自定义的tabbar
+
 ![图片效果演示](https://github.com/githupchenqiang/Tabbar/raw/master/Untitled.gif)
 
